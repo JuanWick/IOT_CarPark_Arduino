fbdb529d7788e81364b15370053d71a582908753
JuanWick <jm.anghileri@wanadoo.fr>
2019-04-17 08:58:04 +0200

    gestionaire de connection wifi

--- Module_WiFiScan/WiFiScan/WiFiScan.ino
+++ Module_WiFiScan/WiFiScan/WiFiScan.ino
@@ -1,73 +1,116 @@
-/*
-    This sketch demonstrates how to scan WiFi networks.
-    The API is almost the same as with the WiFi Shield library,
-    the most obvious difference being the different file you need to include:
-*/
-#include "ESP8266WiFi.h"
-#include "src/WiFi101_hack.h"
-#include "src/common/include/nm_common.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266Ping.h>
 
-// Specify IP address or hostname
-String hostName = "www.google.com";
-int pingResult;
+const char* ssid     = "SFR_6AF8";
+const char* password = "ashi4bbowhibriumanda";
+const char* remote_host = "www.google.com";
+bool is_network_op = false;
+int current_ssid = 0;
 
 void setup() {
   Serial.begin(115200);
-
-  // Set WiFi to station mode and disconnect from an AP if it was previously connected
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(100);
-
   Serial.println("Setup done");
 }
 
 void loop() {
-  pingFreeNetworks();
-  // Wait a bit before scanning again
-  delay(5000);
+  testConnection();
 }
 
-void pingFreeNetworks(){
-  // WiFi.scanNetworks will return the number of networks found
-  int n = WiFi.scanNetworks();
-  Serial.println("scan done: ");
+void connectToNetworks() {
+  while (WiFi.status() != WL_CONNECTED && !is_network_op) {
 
-  if (n == 0) {
-    Serial.println("no networks found");
-  } else {
-    Serial.print(n);
-    Serial.println(" networks found");
-    // Check open networks
-    for (int i = 0; i < n; ++i) {
-      if(WiFi.encryptionType(i) == ENC_TYPE_NONE){
-        Serial.print("<< Free discover : >>");
-        Serial.print(WiFi.SSID(i));
-        //On se connecte
-        WiFi.begin(WiFi.SSID(i));
-        while (WiFi.status() != WL_CONNECTED) {
-          delay(500);
-          Serial.print(".");
-        }
-        Serial.println("");
-        Serial.println("WiFi connected");
-        //On ping
-        Serial.print("Pinging ");
-        Serial.print(hostName);
-        Serial.print(": ");
-        pingResult = WiFi2.ping("www.google.com");
+    Serial.println("-- Try Connecting --");
+    Serial.println();
 
-        if (pingResult >= 0) {
-          Serial.print("SUCCESS! RTT = ");
-          Serial.print(pingResult);
-          Serial.println(" ms");
+    int n = WiFi.scanNetworks();
+
+    if (n == 0) {
+      Serial.println("no networks found");
+    } else {
+      Serial.print(n);
+      Serial.println(" networks found.");
+
+      // Check open networks
+      for (int i = 0; i < n; ++i) {
+        if (WiFi.encryptionType(i) == ENC_TYPE_NONE && !is_network_op) {
+          Serial.println("-----------------------------");
+          Serial.print("Open Network discover : ");
+          Serial.println(WiFi.SSID(i));
+
+          WiFi.begin(WiFi.SSID(i));
+
+          int nbAttempts = 0;
+          while (WiFi.status() != WL_CONNECTED && nbAttempts < 10) {
+            nbAttempts++;
+            delay(500);
+            Serial.print(".");
+          }
+          if (WiFi.status() == WL_CONNECTED) {
+            Serial.println("");
+            Serial.print("WiFi connected with ip ");
+            Serial.println(WiFi.localIP());
+            delay(200);
+            makePingRequest(i);
+          } else {
+            Serial.println();
+            Serial.println("WiFi connection time out");
+            Serial.println("-----------------------------");
+            Serial.println("");
+          }
+
         } else {
-          Serial.print("FAILED! Error code: ");
-          Serial.println(pingResult);
+          if ((WiFi.SSID(i) == ssid) && !is_network_op) {
+            Serial.println("-----------------------------");
+            Serial.print("Known Network discover : ");
+            Serial.println(WiFi.SSID(i));
+            WiFi.begin(ssid, password);
+            int nbAttempts = 0;
+
+            while (WiFi.status() != WL_CONNECTED  && nbAttempts < 10) {
+              delay(100);
+              Serial.print(".");
+            }
+            Serial.println("");
+            Serial.print("WiFi connected with ip ");
+            Serial.println(WiFi.localIP());
+            delay(200);
+            makePingRequest(i);
+          }
         }
-        //On se deconnecte
+        delay(10);
       }
-      delay(10);
     }
   }
+}
+
+bool makePingRequest(int i) {
+  Serial.print("Pinging host ");
+  Serial.println(remote_host);
+
+  if (Ping.ping(remote_host)) {
+    is_network_op = true;
+    current_ssid = i;
+    Serial.println("Success!!");
+    return true;
+  } else {
+    is_network_op = false;
+    Serial.println("Error :(");
+    return false;
+  }
+  Serial.println("-----------------------------");
+  Serial.println("");
+}
+
+void testConnection() {
+  if (!makePingRequest(current_ssid)) {
+    connectToNetworks();
+  }
+  Serial.print("Current Network : ");
+  Serial.println(WiFi.SSID(current_ssid));
+  Serial.println("-----------------------------");
+
+  delay(5000);
 }
