3bda2510ee6f1c73e2e67bc700ff1c5af7de77a8
JuanWick <jm.anghileri@wanadoo.fr>
2019-04-18 13:41:12 +0200

    merge wifi module

--- Module_final/car_park/car_park.ino
+++ Module_final/car_park/car_park.ino
@@ -1,13 +1,21 @@
-#include <NewPingESP8266.h> // Capteur ultrason
+#include <NewPingESP8266.h>
 #include <MPU6050_tockn.h>
 #include <Wire.h>
 #include <Servo.h>
 #include <rgb_lcd.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266Ping.h>
+#include <ESP8266HTTPClient.h>
 
 // WiFi constants
-const char *WIFI_SSID = "AndroidAP0DCF";
-const char *WIFI_PASSWORD = "hyny0433";
-const int WIFI_RETRY_DELAY = 1000;
+const char* WIFI_SSID = "Mfbiya";
+const char* WIFI_PASSWORD = "gptopidq";
+const int WIFI_UNKNOWN_RETRY_DELAY = 10;
+const int WIFI_KNOWN_RETRY_DELAY = 2000;
+const char* PING_HOST = "www.google.com";
+const char* THINGSBOARD_SERVER = "http://demo.thingsboard.io/api/v1/GUmgsqDiPPDMpzsMklM7/telemetry";
+bool is_network_op = false;
+int current_ssid = 0;
 
 // HP constants
 const int PIN_SPEAKER = D4;
@@ -36,13 +44,35 @@ rgb_lcd ecranRGB;
 void setup() {
   Serial.begin(115200);
 
-  setup_led();
-  setup_hp();
-  setup_giro();
-  setup_servo();
   setup_lcd();
+  show_message("Setup", ".....", 255, 175, 31);
+  show_message("Setup", "WIFI", 255, 175, 31);
+  setup_wifi();
+  if (is_network_op) {
+    show_message("Setup", "WIFI FOUND", 255, 175, 31);
+  } else {
+    show_message("Setup", "WIFI NOT FOUND", 255, 175, 31);
+  }
+  show_message("Setup", "LED", 255, 175, 31);
+  setup_led();
+  show_message("Setup", "SOUND", 255, 175, 31);
+  setup_hp();
+  show_message("Setup", "GIRO", 255, 175, 31);
+  setup_giro();
+  show_message("Setup", "SERVO", 255, 175, 31);
+  setup_servo();
+  show_message("Setup done", ":-)", 255, 175, 31);
 
   Serial.println("Setup done.");
+}
+
+/**
+   Setup WIFI mode.
+*/
+void setup_wifi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  connect_to_available_networks();
 }
 
 /**
@@ -50,6 +80,18 @@ void setup() {
 */
 void setup_led() {
   pinMode(PIN_LED, OUTPUT);
+
+  digitalWrite(PIN_LED, HIGH);
+  delay(50);
+  digitalWrite(PIN_LED, LOW);
+  delay(50);
+  digitalWrite(PIN_LED, HIGH);
+  delay(50);
+  digitalWrite(PIN_LED, LOW);
+  delay(50);
+  digitalWrite(PIN_LED, HIGH);
+  delay(50);
+  digitalWrite(PIN_LED, LOW);
 }
 
 /**
@@ -57,6 +99,10 @@ void setup_led() {
 */
 void setup_hp() {
   pinMode(PIN_SPEAKER, OUTPUT);
+
+  digitalWrite(PIN_SPEAKER, HIGH);
+  delay(100);
+  digitalWrite(PIN_SPEAKER, LOW);
 }
 
 /**
@@ -73,6 +119,11 @@ void setup_giro() {
 void setup_servo() {
   servo1.attach(D6);
   servo1.write(90);
+  delay(500);
+  servo1.write(0);
+  delay(500);
+  servo1.write(90);
+
 }
 
 /**
@@ -80,6 +131,16 @@ void setup_servo() {
 */
 void setup_lcd() {
   ecranRGB.begin(16, 2, 0x00);
+}
+
+void show_message(char* message_haut, char* message_bas, int red, int green, int blue) {
+  ecranRGB.clear();
+  ecranRGB.setRGB(red, green, blue);
+  ecranRGB.setCursor(0, 0);
+  ecranRGB.print(message_haut);
+  ecranRGB.setCursor(0, 1);
+  ecranRGB.print(message_bas);
+  delay(2000);
 }
 
 /**
@@ -117,6 +178,9 @@ void impact_event() {
   servo1.write(0);
   delay(700);
   servo1.write(90);
+
+  internet_connection_handler();
+  send_colide_state();
 }
 
 /**
@@ -160,13 +224,12 @@ bool did_moved(int _x) {
 }
 
 /**
-   ESP event loop.
+   Handler Sonar feedback to LCD and LED
 */
-void loop() {
+void distance_detection_handler() {
   distance = get_distance();
   unsigned long currentTime = millis();
 
-  //Gestion de la distance à un obstacle
   if (currentTime - prev > distance * 10 && distance > 0 && distance <= 25) {
     prev = currentTime;
     if (sState == LOW && distance != 0) {
@@ -178,13 +241,175 @@ void loop() {
     play_tone(distance, 20 / distance * 1500);
     print_message_distance(distance);
   }
+}
 
-  //Gestion d'une colision
-  if (millis() - timeStamp > 2) {
+/**
+   Handle GIRO detection and feedback
+*/
+void colision_detection_handler() {
+  if (millis() - timeStamp > 10) {
     mpu6050.update();
     if (did_moved(mpu6050.getAccZ()) == true) {
       impact_event();
     }
     timeStamp = millis();
   }
+}
+
+/**
+   Check and handle auto-internet connection
+*/
+void internet_connection_handler() {
+  make_ping_request(current_ssid);
+  if (!is_network_op) {
+    connect_to_available_networks();
+  }
+  Serial.print("Current Network : ");
+  Serial.println(WiFi.SSID(current_ssid));
+  Serial.println("-----------------------------");
+
+  delay(5000);
+}
+
+/**
+   Test current internet connection
+*/
+void make_ping_request(int i) {
+  Serial.print("Pinging host ");
+  Serial.println(PING_HOST);
+
+  if (Ping.ping(PING_HOST)) {
+    is_network_op = true;
+    current_ssid = i;
+    Serial.println("Success!!");
+  } else {
+    is_network_op = false;
+    Serial.println("Error :(");
+  }
+  Serial.println("-----------------------------");
+  Serial.println("");
+}
+
+/**
+   Scan and try to connect to any open network or known wifi network
+*/
+void connect_to_available_networks() {
+  while (WiFi.status() != WL_CONNECTED && !is_network_op) {
+
+    Serial.println("-- Try Connecting --");
+    Serial.println();
+
+    int n = WiFi.scanNetworks();
+
+    if (n == 0) {
+      Serial.println("no networks found");
+    } else {
+      Serial.print(n);
+      Serial.println(" networks found.");
+
+      // Check open networks
+      for (int i = 0; i < n; ++i) {
+        if (WiFi.encryptionType(i) == ENC_TYPE_NONE && !is_network_op) {
+          Serial.println("-----------------------------");
+          Serial.print("Open Network discover : ");
+          Serial.println(WiFi.SSID(i));
+
+          WiFi.begin(WiFi.SSID(i));
+
+          int nbAttempts = 0;
+          while (WiFi.status() != WL_CONNECTED && nbAttempts < WIFI_UNKNOWN_RETRY_DELAY) {
+            nbAttempts++;
+            delay(500);
+            Serial.print(".");
+          }
+          if (WiFi.status() == WL_CONNECTED) {
+            Serial.println("");
+            Serial.print("WiFi connected with ip ");
+            Serial.println(WiFi.localIP());
+            delay(200);
+            make_ping_request(i);
+          } else {
+            Serial.println();
+            Serial.println("WiFi connection time out");
+            Serial.println("-----------------------------");
+            Serial.println("");
+          }
+
+        } else {
+          if ((WiFi.SSID(i) == WIFI_SSID) && !is_network_op) {
+            Serial.println("-----------------------------");
+            Serial.print("Known Network discover : ");
+            Serial.println(WiFi.SSID(i));
+            WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+            int nbAttempts = 0;
+
+            while (WiFi.status() != WL_CONNECTED  && nbAttempts < WIFI_KNOWN_RETRY_DELAY) {
+              nbAttempts++;
+              delay(100);
+              Serial.print(".");
+            }
+            Serial.println("");
+            Serial.print("WiFi connected with ip ");
+            Serial.println(WiFi.localIP());
+            delay(200);
+            make_ping_request(i);
+          }
+        }
+        delay(10);
+      }
+    }
+  }
+}
+
+/**
+   Send current state information
+*/
+void send_state(String payload) {
+  HTTPClient http;
+  http.begin(THINGSBOARD_SERVER);
+  http.POST(payload);
+  http.end();
+
+  Serial.println();
+  Serial.println("-----------------------------");
+  Serial.println("request sent");
+  Serial.print(" STATE : ");
+  Serial.println(payload);
+  Serial.println("-----------------------------");
+  Serial.println();
+}
+
+/**
+   Inform back-end of connection state
+*/
+void send_connection_state() {
+  if ((WiFi.status() == WL_CONNECTED) && is_network_op) {
+    String payload = "{";
+    payload += "\"isConnected\":";
+    payload += is_network_op;
+    payload += "}";
+    send_state(payload);
+  }
+}
+
+/**
+   Inform back-end of colide event
+*/
+void send_colide_state() {
+  if ((WiFi.status() == WL_CONNECTED) && is_network_op) {
+    String payload = "{";
+    payload += "\"collideEvent\":";
+    payload += true;
+    payload += "}";
+    send_state(payload);
+  }
+}
+
+/**
+   ESP event loop.
+*/
+void loop() {
+  distance_detection_handler();
+  colision_detection_handler();
+  send_connection_state();
 }
